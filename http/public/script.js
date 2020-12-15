d4391eeba10c68372fee219861093bc0fc7bac8e
Rafael <rafaeljuliani1984@gmail.com>
2020-12-14 23:15:55 -0300

    feat: Updated style and API data consumed on frontend

--- http/public/script.js
+++ http/public/script.js
@@ -3,45 +3,57 @@ const input = document.querySelector("input");
 const form = document.querySelector('form');
 
 
+async function load() {
+  const res = await fetch("http://localhost:3000/")
+    .then(data => data.json())
+    .then(data => {
+      data.urls.map(url => {
+        addElement(url);
+      })
+    });
+}
+
+load();
+
 function addElement({ name, url }) {
-    const li = document.createElement('li');
-    const a = document.createElement("a");
-    const trash = document.createElement("span");
+  const li = document.createElement('li');
+  const a = document.createElement("a");
+  const trash = document.createElement("span");
 
-    a.href = url;
-    a.innerHTML = name;
-    a.target = "_blank";
+  a.href = url;
+  a.innerHTML = name;
+  a.target = "_blank";
 
-    trash.innerHTML = "x";
-    trash.onclick = () => removeElement(trash);
+  trash.innerHTML = "x";
+  trash.onclick = () => removeElement(trash);
 
-    li.append(a);
-    li.append(trash);
-    ul.append(li);
+  li.append(a);
+  li.append(trash);
+  ul.append(li);
 }
 
 function removeElement(el) {
-    if (confirm('Tem certeza que deseja remover?'))
-        el.parentNode.remove();
+  if (confirm('Tem certeza que deseja remover?'))
+      el.parentNode.remove();
 }
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    let { value } = input;
+  let { value } = input;
 
-    if (!value) 
-        return alert('Preencha o campo!');
+  if (!value) 
+      return alert('Preencha o campo!');
 
-    const [name, url] = value.split(",");
+  const [name, url] = value.split(",");
 
-    if (!url) 
-        return alert('Formate o texto da maneira correta!');
+  if (!url) 
+      return alert('Formate o texto da maneira correta!');
 
-    if (!/^http/.test(url)) 
-        return alert("Digite a url da maneira correta!");
+  if (!/^http/.test(url)) 
+      return alert("Digite a url da maneira correta!");
 
-    addElement({ name, url });
+  addElement({ name, url });
 
-    input.value = "";
+  input.value = "";
 })
