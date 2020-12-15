87822337ce0b92813bfcb17069586cca219d687c
Rafael <rafaeljuliani1984@gmail.com>
2020-12-14 21:50:03 -0300

    feat: Add style.css and script.js to server

--- http/server.js
+++ http/server.js
@@ -11,19 +11,25 @@ http.createServer((req, res) => {
 
   let { url } = req;
 
-  if(url === '/') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'index.html'),
-      (err, content) => {
+  const file = url === '/' ? 'index.html' : url
+  const filePath = path.join(__dirname, 'public', file);
+  const extname = path.extname(filePath);
+
+  const allowedFileTypes = ['.html', '.css', '.js'];
+  const allowed = allowedFileTypes.find(item => item == extname);
+
+  if(!allowed) return;
+
+  fs.readFile(
+    filePath,
+    (err, content) => {
         if(err) throw err;
-
         res.end(content);
-      }
-    );
-  }
+    }
+  )
 
-  if(url === '/contato') {
-    return res.end('<h1>Contato</h1>');
-  }
+  // if(url === '/contato') {
+  //   return res.end('<h1>Contato</h1>');
+  // }
 
 }).listen(port, () => console.log(`Server is running on http://localhost:${port}`))
